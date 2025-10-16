40fc9e686c00d2a29d601eac02376d39d1319a41
Jackthin <jackthin@foxmail.com>
2025-10-16 20:23:35 +0800

    Lesson 5 Homework

--- Lesson 5-10.13/Homework3.7.c
+++ Lesson 5-10.13/Homework3.7.c
@@ -4,3 +4,20 @@
 //  #define PI 3.1415927
 //  double r=1.5,h=3.0;
 //  小数点后6位
+
+#include <stdio.h>
+#define PI 3.1415927
+
+int main() {
+    double r=1.5, h=3.0;
+    printf("请输入圆的半径r: ");
+    scanf("%lf", &r);
+    printf("请输入圆柱的高h: ");
+    scanf("%lf", &h);
+    printf("圆周长：%.6f\n", 2*PI*r);
+    printf("圆面积：%.6f\n", PI*r*r);
+    printf("圆球表面积：%.6f\n", 4*PI*r*r);
+    printf("圆球体积：%.6f\n", 4*PI*r*r*r/3.0);
+    printf("圆柱体积：%.6f\n", PI*r*r*h);
+    return 0;
+}
